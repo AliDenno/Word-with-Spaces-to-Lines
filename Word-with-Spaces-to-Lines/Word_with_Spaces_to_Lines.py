60f098df4ebe5655dfca2bfa3fe20d8220d1c7e3
Ali Denno <aliedenno@gmail.com>
2017-08-13 21:50:41 +0200

    Add writing mechanism

--- Word-with-Spaces-to-Lines/Word_with_Spaces_to_Lines.py
+++ Word-with-Spaces-to-Lines/Word_with_Spaces_to_Lines.py
@@ -1,4 +1,12 @@
 
-print("let's start")
+print("Start")
 channel_output_file="text8"
 channel_values = open(channel_output_file).read().split()
+
+
+f = open('words.txt', 'w')
+for word in channel_values:
+    f.write(word+'\n')
+f.close()
+
+print("Done")
